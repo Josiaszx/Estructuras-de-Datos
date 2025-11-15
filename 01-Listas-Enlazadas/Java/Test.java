35e251a2706d1880b2f7113f069a1d92f3238175
Josias <maidanajosias220@gmail.com>
2025-11-15 17:15:58 -0300

    lista enlazada implementada en Java

--- 01-Listas-Enlazadas/Java/Test.java
+++ 01-Listas-Enlazadas/Java/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        
         var lista = new ListaEnlazada<String>();
 
         // Agregar elementos
@@ -11,11 +12,13 @@ public class Test {
         lista.agg("Juana", 0);
         lista.agg("Andrea", 4);
 
-        System.out.println(lista);
-
+        System.out.println(lista); // --> Juana -> Juan -> Marco -> Pedro -> Andrea
+        
         lista.elim(0);
-        System.out.println(lista);
+        System.out.println(lista); // --> Juan -> Marco -> Pedro -> Andrea
 
-        System.out.println(lista.buscar("Juana"));
+        System.out.println(lista.buscar("Juana")); // -1
+
+        System.out.println(lista.obtener(2)); // --> Pedro
     }
 }
